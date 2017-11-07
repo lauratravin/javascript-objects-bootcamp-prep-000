0acedd9516f7ca6cd7479ff8c7e8d21bddf2a4ac
Laura Travin <laura.travin@gmail.com>
2017-11-07 00:46:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,5 @@ var playlist = new Object ({ madona : 'La Isla bonita', queen : 'Radio Ga Ga' })
 
 
 function updatePlaylist(obj, key, value){
-  return  Object.assign ({},obj, {[key] : value});    
-} 
+  return  Object.assign ({},obj, {[key] : value});
+}
